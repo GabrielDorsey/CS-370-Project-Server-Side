0e46ec12c793de6bcf8a6bdb89dfe074581351e9
GabrielDorsey <gcd0003@uah.edu>
2022-11-24 12:25:48 -0600

    Commented out code and updated the GUI for further implementation.

--- src/serverSide.java
+++ src/serverSide.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 
 public class serverSide extends JFrame{
 
 
     private JTextField textBox;
     private JButton sendButton;
-    private JButton sendFileButton;
     private JTextArea chatArea;
     private JPanel mainPanel;
     private  static Socket socket;
@@ -18,11 +18,20 @@ public class serverSide extends JFrame{
     private  static BufferedWriter buffWriter;
     private static  BufferedReader buffReader;
     private static ServerSocket serverSocket;
+
+    /**
+     * @param title gets the title so that the frame can update it when the program is ran.
+     *              Function: Creates the server side UI to be used throughout the program
+     *
+     */
     public serverSide(String title){
+        //Gets the title of the fram
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+
+        //When the send message button is pressed, send the message to the client
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -33,12 +42,21 @@ public class serverSide extends JFrame{
 
                 }
                 else {
-                    System.out.println("Could not retrive");
+                    System.out.println("Could not retrieve");
                 }
             }
         });
     }
 
+
+    /**
+     * Method: closeConnection
+     * Function: Closes the connection of all of the thing that the server uses, and throws and exception if it does not work
+     * @param socket  Used to close the socket connection
+     * @param serverSocket Used to close the serverSocket connection
+     * @param buffReader Used to close the reader Connection
+     * @param buffWriter Used to close the writer
+     */
     public static void closeConnection(Socket socket,ServerSocket serverSocket,  BufferedReader buffReader, BufferedWriter buffWriter){
         try{
             if(socket!= null){
@@ -57,8 +75,15 @@ public class serverSide extends JFrame{
     }
 
 
+    /**
+     * Method: sendMessage
+     * Function: Sends a message to the client
+     * @param message : Message to send to the client
+     */
     public void sendMessage(String message){
+        //Attempts to send the message to the client by writing to the buffer and flushes the buffer out, throws and exception if it cannot.
         try{
+
             buffWriter.write(message);
             buffWriter.newLine();
             buffWriter.flush();
@@ -68,29 +93,38 @@ public class serverSide extends JFrame{
             System.out.println("Could Not Send Message.");
         }
     }
+
+
+    /**
+     * Main Method that handles the runtime of the program
+     * Function: Will load the JFrame so that the user can type out to the screen and send messages.
+     * Will attempt to establish a connection to the client so that it can send messages back and forth to one another.
+     */
     public static void main(String[ ] args) {
 
-        serverSide mainFrame =  new serverSide("Client Side App");
+        serverSide mainFrame =  new serverSide("Server Side Application");
         mainFrame.setVisible(true);
-
+        //Attempts to establish a connection the client so that messages can be sent back and forth
         try {
             try {
+                //Creates all of the required tools to send messages
                 serverSocket = new ServerSocket(12345);
-                socket = serverSocket.accept();
+                socket = serverSocket.accept(); //Establishes the connection to the client
+                //Creates a buffered reader and writer so that the application can send and receive messages
                 buffReader = new BufferedReader( new InputStreamReader(socket.getInputStream()));
                 buffWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            } catch(IOException e){
+            } catch(IOException e){ //If there is an exception, close the connection
                 e.printStackTrace();
                 closeConnection(socket,serverSocket, buffReader, buffWriter);
             }
 
 
-
+            //While the connection is still establish, get the client's message and update the textbox.
             while(true) {
                 String fromClient;
 
-                while((fromClient = buffReader.readLine()) != null){
+                while(!Objects.equals(fromClient = buffReader.readLine(), "null")){ //While the object does not equal null, update the chat box.
                     fromClient = buffReader.readLine();
                     mainFrame.chatArea.setText(mainFrame.chatArea.getText().trim()+ "\n Client:\t" + fromClient);
 
